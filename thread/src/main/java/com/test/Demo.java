9ba265b186d08f793e18bde5e0c683fef06e29e8
yida <15268179013@139.com>
2019-08-29 10:43:39 +0800

    测试

--- thread/src/main/java/com/test/Demo.java
+++ thread/src/main/java/com/test/Demo.java
@@ -1,7 +1,6 @@
 package com.test;
 
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.UUID;
 
 /**
  * Created by stack on 2019/4/1.
@@ -9,26 +8,40 @@ import java.util.concurrent.locks.ReentrantLock;
 public class Demo {
 
     public static void main(String[] args) {
-        ReentrantLock reentrantLock = new ReentrantLock(true);
-        Thread thread = new Thread(() -> {
-            while (true) {
-                try {
-                    TimeUnit.SECONDS.sleep(1);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+
+        for (int i = 0; i < 50; i++) {
+
+            new Thread(() -> {
+                for (int j = 0; j < 10000000; j++) {
+                    System.out.println(UUID.randomUUID().toString());
                 }
-                System.out.println("----睡眠一秒-----");
-            }
-        });
-        //默认为false,设置为false代表非守护线程,true为守护线程,守护线程在主方法结束时候结束
-        thread.setDaemon(true);
-        thread.start();
-        try {
-            TimeUnit.SECONDS.sleep(3);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            }).start();
+
         }
-        System.out.println("主线程over");
+
+        while (true) {
+        }
+
+//        ReentrantLock reentrantLock = new ReentrantLock(true);
+//        Thread thread = new Thread(() -> {
+//            while (true) {
+//                try {
+//                    TimeUnit.SECONDS.sleep(1);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//                System.out.println("----睡眠一秒-----");
+//            }
+//        });
+//        //默认为false,设置为false代表非守护线程,true为守护线程,守护线程在主方法结束时候结束
+//        thread.setDaemon(true);
+//        thread.start();
+//        try {
+//            TimeUnit.SECONDS.sleep(3);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        System.out.println("主线程over");
     }
 
 }
